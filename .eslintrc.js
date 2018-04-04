82c8050444e7ab4b933b2201f5b37ea834df326a
srbberlin <info@hans-schrieber.de>
2018-04-04 17:55:23 +0200

    rcs and node_modules local

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "angular": true,
+        "React": true,
+        "ReactDom": true,
+        "d3": true,
+        "topojson": true,
+        "__dirname": true
+    },
+    "rules": {
+        "no-console":0,
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
